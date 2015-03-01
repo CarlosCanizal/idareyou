54c85ff564bf5e12dc4ba958ccd61a9ca8f1f315
Carlos Canizal <carlos@codecery.com>
2015-03-01 08:15:38 -0800

    Resdirects when user is logged in

--- src/client/app/core/initialize.js
+++ src/client/app/core/initialize.js
@@ -2,9 +2,20 @@ angular
 .module('app')
 .run(run);
 
-run.$inject = ['$rootScope', '$state','parse'];
+run.$inject = ['$rootScope', '$state', 'userApi'];
 
-function run($rootScope, $state, parse) {
-  
+function run($rootScope, $state, userApi) {
+  $rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
+    if(toState.data && toState.data.access && !userApi.currentUser()){
+      event.preventDefault();
+      $state.go('user');
+    }
+
+    if(toState.name == "user" && userApi.currentUser()){
+      event.preventDefault();
+      $state.go('challenges');
+    }
+
+  });
 }
 
